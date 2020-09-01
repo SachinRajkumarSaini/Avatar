a4c60a6c2cd1a5b943a5905f995551614374bd59
SachinRajkumarSaini <sachinrajkumarsaini2002@gmail.com>
2020-09-01 20:33:24 +0530

    Updates

---
+++ precache-manifest.3a1586867313a1ff16e3d4264eb12d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a58cc36b23656c552e8f8704d8ccdad",
+    "url": "/Avatar/index.html"
+  },
+  {
+    "revision": "9f3a23075b5c0f7d11f7",
+    "url": "/Avatar/static/css/2.78e0feef.chunk.css"
+  },
+  {
+    "revision": "03ea667c2e7021feba1b",
+    "url": "/Avatar/static/css/main.57f5715d.chunk.css"
+  },
+  {
+    "revision": "9f3a23075b5c0f7d11f7",
+    "url": "/Avatar/static/js/2.f771f158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Avatar/static/js/2.f771f158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ea667c2e7021feba1b",
+    "url": "/Avatar/static/js/main.0f6d96f9.chunk.js"
+  },
+  {
+    "revision": "f6766008600847656388",
+    "url": "/Avatar/static/js/runtime-main.05f379dd.js"
+  }
+]);
